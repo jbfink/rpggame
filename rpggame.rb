5591c88a073bd3f6fd46bc4aa18c2486523beb3a
John Fink <john.fink@gmail.com>
2010-07-14 09:40:57 -0400

    Added rpggame.rb, class Character and stats.

---
+++ rpggame.rb
@@ -0,0 +1,14 @@
+class Character
+   def initialize(name, strength, dexterity, constitution, intelligence, wisdom, charisma, alignment, cclass, race)
+	@name = name
+	@strength = strength
+	@dexterity = dexterity
+	@constitution = constitution
+	@intelligence = intelligence
+	@wisdom = wisdom
+	@charisma = charisma
+	@alignment = alignment
+	@cclass = cclass
+	@race = race
+	end
+  end
