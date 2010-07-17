1ab5fb564de9731ea2f6498c4713b909a7095a83
John Fink <jbfink@zamenhof>
2010-07-17 11:09:28 -0400

    Added a smaller class because I'm lazy.

--- rpggame.rb
+++ rpggame.rb
@@ -1,4 +1,11 @@
 #!/usr/bin/env ruby
+# note that you can't use class as a class variable.  duh.  hence, cclass.  Watch for reserved names!
+class Test
+   def initialize(name, occupation)
+   @name = name
+   @occupation = occupation
+  end
+end
 class Character
    def initialize(name, strength, dexterity, constitution, intelligence, wisdom, charisma, alignment, cclass, race)
 	@name = name
