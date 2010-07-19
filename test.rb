9d9093ee2df0f469cf5c9ae7cce9df2e3fbccf5b
John Fink <jbfink@zamenhof>
2010-07-18 21:35:19 -0400

    More test.rb cleanup.

--- test.rb
+++ test.rb
@@ -1,15 +1,13 @@
 #!/usr/bin/env ruby
-# note that you can't use class as a class variable.  duh.  hence, cclass.  Watch for reserved names!
 class Test
    def initialize(name, occupation)
    @name = name
    @occupation = occupation
    def sheet
       "This is #@name, who is a/an #@occupation"
-#wtf is this shit?  Only the last def picks up on the instance variables?  Why???
-end
-def to_a
-%w{#@name #@occupation}
+def sheetplus
+     "This is #@name, who is a/an #@occupation, but why does this method succeed where the previous one fails?"
 end
 end
   end
+end
