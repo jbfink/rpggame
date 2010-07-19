a738990297d513e9c34b96799fa6adc1bb32aa24
John Fink <jbfink@zamenhof>
2010-07-18 20:30:11 -0400

    Grrr.  I know I'm missing something obvious.

--- test.rb
+++ test.rb
@@ -8,7 +8,8 @@ class Test
       "This is #@name, who is a/an #@occupation"
 end
 def to_a
-    #almost there, I swear it.
+#the problem is what, that the @variables are not visible outside the initialize method? right?
+ %w{#@name #@occupation}
 end
   end
 end
