39a22682ae796c703e95f62e0713364175ea6e82
John Fink <jbfink@zamenhof>
2010-07-18 22:51:23 -0400

    Fixed in test.rb.  Close your methods, man!  Thanks much to stackoverflow for showing me the light.

--- test.rb
+++ test.rb
@@ -3,11 +3,11 @@ class Test
    def initialize(name, occupation)
    @name = name
    @occupation = occupation
+end
    def sheet
       "This is #@name, who is a/an #@occupation"
+end
 def sheetplus
      "This is #@name, who is a/an #@occupation, but why does this method succeed where the previous one fails?"
 end
-end
-  end
 end
