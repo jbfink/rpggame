3d9f0218c76491ea64a7ba18a8b2ad0346a7682e
John Fink <john.fink@gmail.com>
2010-07-20 09:43:18 -0400

    Hey, a class variable!  Neat.

--- test.rb
+++ test.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 class Test
+   @@version = 0.01
    def initialize(name, occupation)
    @name = name
    @occupation = occupation
@@ -9,5 +10,8 @@ end
 end
 def sheetplus
      "This is #@name, who is a/an #@occupation, but why does this method succeed where the previous one fails?"
+end
+def version
+    "This is test.rb, version #@@version"
 end
 end
